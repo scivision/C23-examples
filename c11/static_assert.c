1042494204a80ddc3070b73f729899ca160ecdb5
Michael Hirsch <scivision@users.noreply.github.com>
2022-12-16 11:25:17 -0500

    simplify

--- c11/static_assert.c
+++ c11/static_assert.c
@@ -2,9 +2,15 @@
 
 #include <assert.h>
 
+#ifdef _WIN32
+  #define _LINT 4
+#else
+  #define _LINT 8
+#endif
+
 int main(void) {
 
-  static_assert(sizeof(long) == 8, "long is not 8 bytes on this platfrom");
+  static_assert(sizeof(long) == _LINT, "long not expected bytes on this platform");
 
   return 0;
 }
