32a52f6b1ecb3883ac9b2460560003e5ba04291e
Michael Hirsch <scivision@users.noreply.github.com>
2022-12-16 11:18:58 -0500

    add c23 nullptr

---
+++ c23/nullptr.c
@@ -0,0 +1,39 @@
+// https://en.cppreference.com/w/c/language/nullptr
+
+#include <stddef.h>
+#include <stdio.h>
+
+void g(int*)
+{
+    puts("Function g called");
+}
+
+#define DETECT_NULL_POINTER_CONSTANT(e) \
+    _Generic(e,                         \
+        void* : puts("void*"),          \
+        nullptr_t : puts("nullptr_t"),  \
+        default : puts("integer")       \
+    )
+
+int main(void)
+{
+    g(nullptr);        // Fine
+    g(NULL);           // Fine
+    g(0);              // Fine
+
+    puts("----------------");
+
+    auto cloned_nullptr = nullptr;
+    auto cloned_NULL   = NULL;
+    auto cloned_zero   = 0;
+    g(cloned_nullptr); // Fine
+//  g(cloned_NULL);    // ERROR
+//  g(cloned_zero);    // ERROR
+
+    puts("----------------");
+
+    DETECT_NULL_POINTER_CONSTANT(((void*)0));
+    DETECT_NULL_POINTER_CONSTANT(0);
+    DETECT_NULL_POINTER_CONSTANT(nullptr);
+    DETECT_NULL_POINTER_CONSTANT(NULL); // implementation-defined
+}
