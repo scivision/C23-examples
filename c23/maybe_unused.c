ba52f91f2bb3f732855bb5376f7008b387c5ac92
scivision <scivision@users.noreply.github.com>
2023-02-05 00:31:31 -0500

    add maybe_unused

---
+++ c23/maybe_unused.c
@@ -0,0 +1,20 @@
+// https://en.cppreference.com/w/c/language/attributes/maybe_unused
+#include <assert.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+[[maybe_unused]] void f([[maybe_unused]] _Bool cond1, [[maybe_unused]] _Bool cond2)
+{
+   [[maybe_unused]] _Bool b = cond1 && cond2;
+   assert(b); // in release mode, assert is compiled out, and b is unused
+              // no warning because it is declared [[maybe_unused]]
+} // parameters cond1 and cond2 are not used, no warning
+
+int main(void)
+{
+  f(1, 1);
+
+  printf("OK: maybe_unused\n");
+  return EXIT_SUCCESS;
+}
