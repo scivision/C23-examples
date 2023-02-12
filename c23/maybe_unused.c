500ad53d91bcb947ba757e4004e684fdd62ff1fb
scivision <scivision@users.noreply.github.com>
2023-02-12 15:28:31 -0500

    maybe_Unused: better check

--- c23/maybe_unused.c
+++ c23/maybe_unused.c
@@ -3,12 +3,14 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 [[maybe_unused]] void f([[maybe_unused]] _Bool cond1, [[maybe_unused]] _Bool cond2)
 {
-   [[maybe_unused]] _Bool b = cond1 && cond2;
-   assert(b); // in release mode, assert is compiled out, and b is unused
-              // no warning because it is declared [[maybe_unused]]
+   [[maybe_unused]] bool b = cond1 && cond2;
+   assert(b);
+   // in release mode, assert is compiled out, and b is unused
+   // no warning because it is declared [[maybe_unused]]
 } // parameters cond1 and cond2 are not used, no warning
 
 int main(void)
