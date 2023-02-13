3659b3a06dd11c947c3bc1079cff73c59986cb84
scivision <scivision@users.noreply.github.com>
2023-02-12 23:22:16 -0500

    add getenv

---
+++ c99/getenv.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _MSC_VER
+#include <crtdbg.h>
+#endif
+
+int main(void)
+{
+#ifdef _MSC_VER
+  _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+  _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+  _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+#endif
+
+  size_t MAXP = 32768;
+  size_t L;
+  char* name = "PATH";
+
+  char* buf;
+  buf = getenv(name);
+  if(!buf){
+    fprintf(stderr, "ERROR:getenv\n");
+    return EXIT_FAILURE;
+  }
+
+  printf("%s: %s\n", name, buf);
+
+  return EXIT_SUCCESS;
+}
